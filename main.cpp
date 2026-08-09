cc7afead4d0d2da0a3c950f5a85353868aee1773
lukewarm1006 <lukeahumphris@gmail.com>
2026-08-09 17:44:08 +0930

    added main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// COMP 2017 - Practical 1
+//
+// I declare this submission is my own work and has not been copied from any other source.
+// Name: PUT YOUR NAME HERE
+// a#: PUT YOUR a# HERE
+//
+
+#include <iostream>
+
+int main() {
+    int n;
+    std::cin >> n;      // read an integer from input stream
+    std::cout << n;     // print the result to output stream
+    return 0;
+}
+
