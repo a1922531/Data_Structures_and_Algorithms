fc3524aa8bd02fd93b7196a106370d715bb72380
lukewarm1006 <lukeahumphris@gmail.com>
2026-08-21 11:41:53 +0930

    changed it

--- Finder.cpp
+++ Finder.cpp
@@ -3,30 +3,18 @@
 using namespace std;
 
 vector<int> Finder::findSubstrings(string s1, string s2) {
-    const size_t n = s2.size(), m = s1.size();
-    vector<int> result(n, -1);
-    if (n == 0) return result;
+  vector<int> result;
+  result.reserve(s2.size());
 
-    // prefix function (failure table) of s2
-    vector<size_t> fail(n, 0);
-    for (size_t i = 1; i < n; ++i) {
-        size_t k = fail[i - 1];
-        while (k > 0 && s2[i] != s2[k]) k = fail[k - 1];
-        if (s2[i] == s2[k]) ++k;
-        fail[i] = k;
+  size_t pos = 0;
+
+  for (size_t i = 1; i <= s2.size(); i++) {
+    pos = s1.find(s2.data(), pos, i);
+    if (pos == string::npos) {
+      result.resize(s2.size(), -1);
+      break;
     }
-
-    size_t k = 0, best = 0;
-    for (size_t j = 0; j < m; ++j) {
-        while (k > 0 && s1[j] != s2[k]) k = fail[k - 1];
-        if (s1[j] == s2[k]) ++k;
-
-        if (k > best) {                              // new longest prefix seen
-            best = k;
-            result[k - 1] = static_cast<int>(j - k + 1);
-            if (best == n) break;                    // nothing longer to find
-        }
-        if (k == n) k = fail[k - 1];
-    }
-    return result;
+    result.push_back(static_cast<int>(pos));
+  }
+  return result;
 }
