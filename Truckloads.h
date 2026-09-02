35fc506f5e0710879fee87f0bd4c5ead07fd436f
lukewarm1006 <lukeahumphris@gmail.com>
2026-09-02 12:26:56 +0930

    done

--- Truckloads.h
+++ Truckloads.h
@@ -2,13 +2,7 @@
 #define TRUCKLOADS_H
 
 class Truckloads {
- private:
-  int numCrates_;
-  int loadSize_;
-
  public:
-  Truckloads(int numCrates, int loadSize) : numCrates_(numCrates), loadSize_(loadSize) {}
-
   int numTrucks(int numCrates, int loadSize);
 };
 
--- main.cpp
+++ main.cpp
@@ -5,13 +5,13 @@
 
 int main() {
     std::cout << "Truckloads tests:" << std::endl;
-    Truckloads test(14, 3);
+    Truckloads test;
     std::cout << test.numTrucks(14, 3) << std::endl;
 
-    Truckloads test2(15, 1);
+    Truckloads test2;
     std::cout << test2.numTrucks(15, 1) << std::endl;
 
-    Truckloads test3(1024, 5);
+    Truckloads test3;
     std::cout << test3.numTrucks(1024, 5) << std::endl;
 
     std::cout << "\nReverser tests:" << std::endl;
